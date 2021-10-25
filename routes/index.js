b6a0fce9a797436232bd16d6db0c7ddb5bc5c2e5
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-10-25 09:24:46 +0300

    api cache

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
+const apicaache = require('apicache');
 
 //Env vars
 
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
-router.get('/', async (req, res)=>{
+
+//Init  cache
+let cache = apicaache.middleware;
+
+router.get('/', cache('2 minutes'),async (req, res)=>{
     try{
         const params = new URLSearchParams({
             [API_KEY_NAME]: API_KEY_VALUE,
