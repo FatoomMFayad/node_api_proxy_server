0ac32af66f0339b26233fd0d0a9e6099a6872c95
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-10-25 12:10:42 +0300

    display temp in c

--- public/main.js
+++ public/main.js
@@ -1,56 +1,60 @@
-const weatehrDisplay = document.querySelector('.weather')
+const weatherDisplay = document.querySelector('.weather')
 const weatherForm = document.querySelector('#weather-form')
 const cityInput = document.querySelector('#city-input')
 
-//Fetch weather data from API
-const fetchWeather = async(city) => {
-    const url = `/api?q=${city}`
+// Fetch weather data from API
+const fetchWeather = async (city) => {
+  const url = `/api?q=${city}`
 
-    const res = await fetch(url);
-    const data = await res.json()
+  const res = await fetch(url)
+  const data = await res.json()
 
-    if(data.cod === '404') {
-        alert('City not found')
-        return 
-    }
+  if (data.cod === '404') {
+    alert('City not found')
+    return
+  }
 
-    if(data.cod === '401'){
-        alert('Invalid API key')
-        return
-    }
+  if (data.cod === 401) {
+    alert('Invalid API Key')
+    return
+  }
 
-    const displayData = {
-        city: data.name,
-        temp: kelvinToFahrenheit(data.main.temp),
-    }
-    addWeatherToDOM(displayData);
+  const displayData = {
+    city: data.name,
+    temp: kelvinToCelsius(data.main.temp),
+  }
 
-    //Add display data to DOM
-    const addWeatherToDOM = (data) => {
-        weatehrDisplay.innerHTML = `
-            <h1>Weather in ${data.city}</h1>
-            <h2>${data.temp} &deg;F</h2>
-        `
-        cityInput.value=''
-    }
-
-    //Convert Kelvin to Fahrenheit
-    const kelvinToFahrenheit = (temp) => {
-        return Math.ceil(((temp - 273.15) * 9) / 5 + 32)
-    }
-
-    //Event listener for form submission
-    weatherForm.addEventListener('submit', (e) =>{
-        e.preventDefault()
-
-        if(cityInput.value === '') {
-            alert('Please enter a city')
-        }else{
-            fetchWeather(cityInput.value)
-        }
-    })
-
-    //Initial Fetch
-    fetchWeather('Miami')
-
+  addWeatherToDOM(displayData)
 }
+
+// Add display data to DOM
+const addWeatherToDOM = (data) => {
+  weatherDisplay.innerHTML = `
+    <h1>Weather in ${data.city}</h1>
+    <h2>${data.temp} &deg;C</h2>
+  `
+  cityInput.value = ''
+}
+
+// Convert Kelvin to Fahrenheit
+const kelvinToFahrenheit = (temp) => {
+  return Math.ceil(((temp - 273.15) * 9) / 5 + 32)
+}
+// Convert Kelvin to Celsius 
+const kelvinToCelsius = (temp) => {
+  return Math.ceil(temp - 273.15);
+}
+
+// Event listener for form submission
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (cityInput.value === '') {
+    alert('Please enter a city')
+  } else {
+    fetchWeather(cityInput.value)
+  }
+})
+
+// Initial fetch
+fetchWeather('Miami')
